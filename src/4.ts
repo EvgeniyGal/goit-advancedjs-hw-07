4be815decfb20336d9ac2c8d15cadc72446b702f
Evgeniy <egeniygal@gmail.com>
2023-12-04 19:46:52 +0200

    All done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,67 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.round(Math.random() * 100000000);
+  }
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      this.door = false;
+    } else {
+      console.log("At first you must open the door!");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key: Key = new Key();
+const person: Person = new Person(key);
+
+const thiefKey: Key = new Key();
+const thief: Person = new Person(thiefKey);
+
+const house: MyHouse = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+console.log(house);
+
+house.openDoor(thief.getKey());
+
+house.comeIn(thief);
 
 export {};
